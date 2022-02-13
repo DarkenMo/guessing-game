f918cc60110d56176b19ab305f1b5160681f38a8
BuildTools <moh.85.amiri@gmail.com>
2022-02-13 20:54:55 +0300

    cleaned up the code a bit and fixed some issues and bugs

--- guessing.py
+++ guessing.py
@@ -1,27 +1,39 @@
-# guess a number between 1 and 10
 import random
+
+score = 0
+
+
+def tryAgain():
+    global score
+    tryAgainTXT = input("try again?(y, n): ")
+    # if they user types y the game restarts and if n or anything else it will end
+    if tryAgainTXT == "y":
+        guessing()
+    elif tryAgainTXT == "n":
+        print("game ended your score was: " + str(score))
+    elif tryAgainTXT != ("y", "n"):
+        print("wrong input try again!")
+        tryAgain()
 
 
 def guessing():
+    global score
     # generates the number to actually guess
     num = random.randint(1, 10)
     # sets the guess to nothing
     guess = None
     guess_count = 0
     while guess != num:
-        guess = input("guess a number between 1 and 10: ")
-        guess = int(guess)
-
+        guess = int(input("guess a number between 1 and 10: "))
         # checks if your guess is the same as the number and if it is it prints out the things below and if its not then it will add 1 to the counter and print that it was wrong
         if guess == num:
             print("congratulations. you won!")
             print("you geussed it in " + str(guess_count) + " tries")
-            tryAgain = input("try again?(y, n): ")
-            # if they user types y the game restarts and if n or anything else it will end
-            if tryAgain == "y":
-                guessing()
-            else:
-                break
+            # adding 10 to the score???
+            score += 1
+            print("current score: " + str(score))
+            tryAgain()
+
         else:
             # checks if the guessed number is lower or higher than the generated number
             if guess < num:
